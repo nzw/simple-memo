265c50e6219dd1130dc81d69b5df5b5e91eddfda
nzw <nzw.dev@gmail.com>
2023-04-18 03:00:08 +0900

    add tab function

--- src/js/load.js
+++ src/js/load.js
@@ -1,3 +1,8 @@
 chrome.storage.local.get('save_content', (items) => {
-    document.getElementById('contents').value = items.save_content || '';
+    let save_contents = items.save_content || '';
+    let textarea_value_list = document.querySelectorAll('.contents');
+    let len = save_contents.length;
+    for (let i = 0; i < len; i++) {
+        textarea_value_list[i].value = save_contents[i];
+    }
 });
--- src/js/save.js
+++ src/js/save.js
@@ -1,7 +1,15 @@
-let textarea = document.getElementById('contents');
-let save = () => {
-    let contents = textarea.value;
-    chrome.storage.local.set({'save_content': contents});
-};
-textarea.addEventListener('keyup', save, false);
-textarea.addEventListener('keydown', save, false);
+setTimeout(() => {
+    let textarea_value_list = document.querySelectorAll('.contents');
+    let len = textarea_value_list.length;
+    let save = () => {
+        let save_contents = [];
+        for (let i = 0; i < len; i++) {
+            save_contents.push(textarea_value_list[i].value);
+        }
+        chrome.storage.local.set({'save_content': save_contents});
+    };
+    for (let i = 0; i < len; i++) {
+        textarea_value_list[i].addEventListener('keyup', save, false);
+        textarea_value_list[i].addEventListener('keydown', save, false);
+    }
+}, 100);
