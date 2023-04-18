f5d999bdded4480f9a72635250c3f046a38f0987
nzw <nzw.dev@gmail.com>
2023-04-19 00:34:03 +0900

    [v1.0] new design & active save

--- src/js/load.js
+++ src/js/load.js
@@ -2,7 +2,8 @@ chrome.storage.local.get('save_content', (items) => {
     let save_contents = items.save_content || '';
     let textarea_value_list = document.querySelectorAll('.contents');
     let len = save_contents.length;
-    for (let i = 0; i < len; i++) {
+    for (let i = 0; i < len - 1; i++) {
         textarea_value_list[i].value = save_contents[i];
     }
+    if (save_contents[len-1] > 0) document.querySelectorAll('[name="cp_tab"]')[save_contents[len-1]-1].checked = true;
 });
--- src/js/save.js
+++ src/js/save.js
@@ -3,9 +3,11 @@ setTimeout(() => {
     let len = textarea_value_list.length;
     let save = () => {
         let save_contents = [];
+        let select_id = document.querySelector('[name="cp_tab"]:checked')['id'];
         for (let i = 0; i < len; i++) {
             save_contents.push(textarea_value_list[i].value);
         }
+        save_contents.push(select_id.replace('tab1_', ''));
         chrome.storage.local.set({'save_content': save_contents});
     };
     for (let i = 0; i < len; i++) {
