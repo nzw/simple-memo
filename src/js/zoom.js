c206134e36f14935faf10fbca9e7b17240a551c7
nzw <nzw.dev@gmail.com>
2023-04-20 20:29:30 +0900

    [v1.2.5] select font size

--- src/js/zoom.js
+++ src/js/zoom.js
@@ -1,10 +1,12 @@
-let zoom_in  = document.querySelector('#zoom-in');
-let zoom_out = document.querySelector('#zoom-out');
+let zoom_in   = document.querySelector('#zoom-in');
+let zoom_out  = document.querySelector('#zoom-out');
+let font_size = document.querySelector('#font-size');
 zoom_in.addEventListener('click', () => {
     let textareas    = document.querySelectorAll('.contents');
     let len          = textareas.length;
     let body         = document.body;
     let textarea_css = 'width: 760px; height: 460px;';
+
     body.style       = 'width: 780px; height: 480px;';
     for (let i = 0; i < len; i++) {
         textareas[i].style = textarea_css;
@@ -14,9 +16,24 @@ zoom_out.addEventListener('click', () => {
     let textareas    = document.querySelectorAll('.contents');
     let len          = textareas.length;
     let body         = document.body;
-    let textarea_css = 'width: 400px; height: 333px;';
+
     body.style       = 'width: 440px; height: 400px;';
+    let textarea_css = 'width: 400px; height: 333px;';
     for (let i = 0; i < len; i++) {
         textareas[i].style = textarea_css;
+    }
+});
+font_size.addEventListener('change', (e) => {
+    let textareas = document.querySelectorAll('.contents');
+    let len       = textareas.length;
+    let body      = document.body;
+    let font      = e.target.value;
+    let size      = font > 14 ? 1 : 0;
+
+    body.style.width  =  size ? 'width: 780px;' : 'width: 440px;';
+    body.style.height =  size ? 'height: 480px;' : 'height: 400px;';
+    for (let i = 0; i < len; i++) {
+        textareas[i].style.fontSize = `${font}em`;
+        if (size) textareas[i].style.width = '760px';
     }
 });
