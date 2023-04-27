2d20781e044a7c22f58c233de16e19b2892d93f4
nzw <nzw.dev@gmail.com>
2023-04-27 23:05:01 +0900

    [v1.3.1] Change font color of tabs with input values

--- src/js/load.js
+++ src/js/load.js
@@ -1,8 +1,12 @@
 chrome.storage.local.get('save_content', (items) => {
+    const tab_set   = 'color:skyblue;font-weight: bold;';
+    const tab_reset = 'color:black;font-weight: normal;';
+    let label_list  = document.querySelectorAll('.cp_tab > label');
     let save_contents = items.save_content || '';
     let textarea_value_list = document.querySelectorAll('.contents');
     let len = save_contents.length;
     for (let i = 0; i < len - 1; i++) {
+        label_list[i].style = save_contents[i] ? tab_set : tab_reset;
         textarea_value_list[i].value = save_contents[i];
     }
     if (save_contents[len-1] > 0) document.querySelectorAll('[name="cp_tab"]')[save_contents[len-1]-1].checked = true;
--- src/js/save.js
+++ src/js/save.js
@@ -1,11 +1,16 @@
 setTimeout(() => {
+    const tab_set   = 'color:skyblue;font-weight: bold;';
+    const tab_reset = 'color:black;font-weight: normal;';
+    let label_list  = document.querySelectorAll('.cp_tab > label');
     let textarea_value_list = document.querySelectorAll('.contents');
     let len = textarea_value_list.length;
     let save = () => {
         let save_contents = [];
         let select_id = document.querySelector('[name="cp_tab"]:checked')['id'];
         for (let i = 0; i < len; i++) {
-            save_contents.push(textarea_value_list[i].value);
+            let val = textarea_value_list[i].value;
+            label_list[i].style = val ? tab_set : tab_reset;
+            save_contents.push(val);
         }
         save_contents.push(select_id.replace('tab1_', ''));
         chrome.storage.local.set({'save_content': save_contents});
